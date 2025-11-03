5d6f6cdd8cefec8c7f66bce6a018676d1ae680d0
chhitijpradhan <chhitijpradhan13@gmail.com>
2025-11-03 21:40:15 +0530

    deployment-cofig-added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,29 @@
 import express from 'express';
+import path from "path";
+
+import { ENV } from './lib/env.js';
+
 const app = express();
-app.get("/", (req, res) => {
-    res.send("HELLO WORLD");
+
+const __dirname = path.resolve();
+app.get("/book", (req,res) => {
+    res.send.status(200).json({
+        message : "book api is working"
+    })
 })
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get("/health", (req, res) => {
+    res.send("api of halth get ");
+})
+
+// maeke ready for deplyoment
+if (ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("/{*any}", (req, res) =>{
+        res.sendFile(path.resolve(__dirname,"../frontend", "dist", "index.html"));
+    })
+}
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
 });
