17a703ccc3c1bd4e3bf34b2fce74689ff95dd74a
chhitijpradhan <chhitijpradhan13@gmail.com>
2025-11-04 10:39:05 +0530

    db conn

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const ENV = {
     PORT : process.env.PORT,
-    DB : process.env.DB_URL,
+    DB_URL : process.env.DB_URL,
     NODE_ENV : process.env.NODE_ENV,
 };
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from "path";
-
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
 
 const app = express();
 
@@ -24,6 +24,16 @@ if (ENV.NODE_ENV === "production"){
         res.sendFile(path.resolve(__dirname,"../frontend", "dist", "index.html"));
     })
 }
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-});
+const startServer = async () =>{
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+        console.log(`Server is running on port ${ENV.PORT}`);
+        })
+    } catch (error) {
+        console.log("Error starting the server", error);
+        process.exit(1);
+
+    }
+};
+startServer();
