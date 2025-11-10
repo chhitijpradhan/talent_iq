0d045be4672bac1af38d26ea5891da58a42eb372
chhitijpradhan <chhitijpradhan13@gmail.com>
2025-11-10 22:28:05 +0530

    questionpage code editor

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -1,8 +1,73 @@
+import { useNavigate, useParams } from "react-router";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import { PROBLEMS } from "../data/problems";
 
-const ProblemPage = () => {
+function ProblemPage() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [currentProblemId, setCurrentProblemId] = useState("two-sum");
+  const [selectedLanguage, setSelectedLanguage] = useState("javascript");
+  const [code, setCode] = useState(PROBLEMS[currentProblemId].starterCode.javascript);
+  const [output, setOutput] = useState(null);
+  const [isRunning, setRunning] = useState(false);
+
+  const currentProblem = PROBLEMS[currentProblemId];
+
+  // update problem when url param changes
+  useEffect(() => {
+    if (id && PROBLEMS[id]) {
+      setCurrentProblemId(id);
+      setCode(PROBLEMS[id].starterCode[selectedLanguage]);
+      setOutput(null);
+    }
+  }, [id, selectedLanguage]);
+
+  const handleLanguageChange = (e) => {
+    const newLang = e.target.value;
+    setSelectedLanguage(newLang);
+    setCode(currentProblem.starterCode[newLang]);
+    setOutput(null);
+  };
+
+  const handleProblemChange = (newProblemId) => navigate(`/problem/${newProblemId}`);
+
+  const triggerConfetti = () => {
+    confetti({
+      particleCount: 80,
+      spread: 250,
+      origin: { x: 0.2, y: 0.6 },
+    });
+
+
+    Confetti({
+      particleCount: 80,
+      spred: 250,
+      origin: { x: 0.2, y: 0.6 },
+    });
+  };
+
+  const normalizedOutput = (output) => {
+    return output
+      .trim()
+      .split("\n")
+      .map((line) =>
+        line
+          .trim()
+          // remove spaces after [ and before ]
+          .replace(/\[\s+/g, "[")
+          .replace(/\s+\]/g, "]")
+          // normalize spaces around commas to single space after comma
+          .replace(/\s*,\s*/g, ",")
+      )
+      .filter((line) => line.length > 0 )
+      .join("\n");
+
+  }
+  
   return <div>
-    <Navbar/>
+    <Navbar />
   </div>;
 };
 
